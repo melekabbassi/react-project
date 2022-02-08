51511d2f3a667a165e449ac43f0bdc3ceb19de0a
Melek <abbassimelek@gmail.com>
2022-02-08 22:35:19 +0100

    learning some things & changed a lot of things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Tweet from "./Tweet"
+import Button from "@mui/material/Button"
 
 function App() {
+  
+  const [users, setUser] = useState([
+    {name: 'Melek', message:'Hello'},
+    {name: "Octo", message:"Hello Back"},
+    {name: "Octopus", message:"AAAA"}
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App">  
+      {users.map(user=>(
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
