bd4fdbecf7c0f6d147389a425a8268398eec440a
Melek <abbassimelek@gmail.com>
2022-04-04 22:09:21 +0100

    find database

--- src/App.js
+++ src/App.js
@@ -1,72 +1,9 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState, useEffect } from 'react';
-import axios from 'axios'
 import NavBar from './components/NavBar';
 import MovieCard from './components/movieCard'
 
 function App() {
-
- /*  // our state hook
-   const [movies, setMovies] = useState([])
-
-   useEffect(() => {
-     console.log('effect')
-     axios
-       .get('http://localhost:3001/movies')
-       .then(response => {
-         console.log('promise fulfilled')
-         setMovies(response.data)
-       })
-   }, [])
-    console.log('render', movies.length, 'movies')
-
-    const hook = () => {
-      axios.get('http://localhost:3001/movies')
-      .then(response => {
-        setMovies(response.data)
-      })
-    }
-    useEffect(hook, [])
-
-const express = require('express')
-const app = express()
-
-let movie = [
-  {
-    id: 1,
-    title: 'Harry Potter',
-    year: 2001,
-    rating: 4.5
-  },
-  {
-    id: 2,
-    title: 'The Dark Knight',
-    year: 2008,
-    rating: 4.5
-  },
-
-  {
-    id: 3,
-    title: 'Inception',
-    year: 2010,
-    rating: 4.5
-  }
-]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Movies</h1>')
-})
-
-app.get('/api/people', (request, response) => {
-  response.json(movie)
-})
-
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})*/
-
 
   return (  
     <div className="navBar">
--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -1,13 +1,58 @@
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 export default function MovieCard() {
 
   const [isOpen, setIsOpen] = useState(false);
 
+  const axios = require("axios");
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        'https://movie-database-alternative.p.rapidapi.com/',
+        {
+          params: {
+            s: 'Avengers Endgame',
+            r: 'json',
+            page: '1'
+          },
+          headers: {
+            'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
+            'X-RapidAPI-Key': 'f0b37ab954mshfc4a8677f14d73ap1d970ejsn10304caf731e'
+          }
+        }
+      )
+      .then(response => {
+        setData(response.data.Search);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  //show movie details
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  //close movie details
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  //render movie details
+  const renderMovieDetails = () => {
+    const { Title, Year, imdbID, Type, Poster } = data;
+    console.log(data);
+  };
+
+
+
+
   return (
     <motion.div transition={{layout: { duration: 1.5, type: "spring"}}} layout="position" className="movieCard" onClick={() => setIsOpen(!isOpen)} >
-      <motion.h2>Framer Motion</motion.h2>
+      <motion.h2>{data.Title}</motion.h2>
       {isOpen && 
       <motion.div className="expand">
         <p>
