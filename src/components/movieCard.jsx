f7f85ab783bc611a30ceaa5f9025414f4d5f86ce
Melek <abbassimelek@gmail.com>
2022-04-11 16:20:22 +0100

    removed firebase

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -1,26 +1,9 @@
 import { motion } from 'framer-motion';
-import { useState, useEffect } from 'react';
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, onValue, updateStarCount} from "firebase/database";
-import firebaseConfig from '../config';
+import { useState } from 'react';
 
 export default function MovieCard() {
 
   const [isOpen, setIsOpen] = useState(false);  
-
-
-  const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-    
-    const db = getDatabase();
-    const starCountRef = ref(db, 'posts/' + db.postId + '/starCount');
-    onValue(starCountRef, (snapshot) => {
-    const data = snapshot.val();
-    //updateStarCount(db.postElement, data);
-    }
-    );
 
   //show movie details
   const handleClick = () => {
@@ -33,24 +16,15 @@ export default function MovieCard() {
     trailer.play();
   };
 
-  //get data from firebase
-  const [movie, setMovie] = useState({});
-  useEffect(() => {
-    const db = getDatabase();
-    const movieRef = ref(db, 'posts/' + db.postId);
-    onValue(movieRef, (snapshot) => {
-      const data = snapshot.val();
-      setMovie(data);
-    });
-  }, []);
-
  
   return (
     <motion.div transition={{layout: { duration: 1.5, type: "spring"}}} layout="position" className="movieCard" onClick={handleClick} >
-      <motion.h2>{movie.title}</motion.h2>
+      <motion.h2>Framer Motion</motion.h2>
       {isOpen && 
       <motion.div className="expand">
-        <motion.img src={movie.image} alt={movie.title} />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam alias incidunt expedita deleniti a qui, animi ea. Ratione vel assumenda neque laudantium dolor eum incidunt, tempore, rem, amet distinctio molestiae.
+        </p>
       </motion.div>}
     </motion.div>
   );
