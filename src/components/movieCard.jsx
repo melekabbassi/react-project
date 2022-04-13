7e7afc4e281210006149228de3594711b34df70a
Melek <abbassimelek@gmail.com>
2022-04-13 16:38:04 +0100

    testing

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -11,17 +11,8 @@ export default function MovieCard() {
   };
  
   return (
-    <motion.div transition={{layout: { duration: 1.5, type: "spring"}}} layout="position" className="movieCard" onClick={handleClick} >
-      <motion.h2>Framer Motion</motion.h2>
-      {isOpen && 
-      <motion.div className="expand">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Ullam alias incidunt expedita deleniti a qui, animi ea. 
-          Ratione vel assumenda neque laudantium dolor eum incidunt, 
-          tempore, rem, amet distinctio molestiae.
-        </p>
-      </motion.div>}
-    </motion.div>
+    <div>
+      
+    </div>
   );
 }
