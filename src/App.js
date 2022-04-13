9b842f649f31e1f975400eba93fc19231766fe92
Melek <abbassimelek@gmail.com>
2022-04-13 16:29:54 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar';
 import MovieCard from './components/movieCard'
 import Form from './components/Form'
+import Home from './components/Home'
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+
 function App() {
+
+  const [movies, setMovies] = useState([])
+  function addMovie(title, year, genre, poster) {
+    const newMovie = {
+      title: title,
+      year: year,
+      genre: genre,
+      poster: poster,
+  };
+  setMovies(movies.concat(newMovie));
+  }
 
   return (  
     <Router>
     <div className="App">
         <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home movies={movies} />
+          </Route>
+          <Route path="/newMovie" component={Form}>
+            <Form addMovie={addMovie} />
+          </Route> 
+        </Switch>
     </div>    
-      
-    <Form />
-  
-
     </Router>
-    
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 export const Home = (props) => {
+  const [isOpen, setIsOpen] = useState(false);  
+
+  //show movie details
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+ 
   return (
-    <div>
-        
-    
-    </div>
-  )
+    <motion.div transition={{layout: { duration: 1.5, type: "spring"}}} layout="position" className="movieCard" onClick={handleClick} >
+      <motion.h2>Framer Motion</motion.h2>
+      {isOpen && 
+      <motion.div className="expand">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Ullam alias incidunt expedita deleniti a qui, animi ea. 
+          Ratione vel assumenda neque laudantium dolor eum incidunt, 
+          tempore, rem, amet distinctio molestiae.
+        </p>
+      </motion.div>}
+    </motion.div>
+  );
 }
+export default Home
